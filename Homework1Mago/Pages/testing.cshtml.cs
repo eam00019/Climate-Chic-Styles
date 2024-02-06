9a7c04df39a599909f453bbb8f247fc06a0f448a
eam00019 <eam00019@mix.wvu.edu>
2024-02-06 13:06:10 -0500

    Add project files.

---
+++ Homework1Mago/Pages/testing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Climate_Chic_Styles.Pages
+{
+    public class testingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
